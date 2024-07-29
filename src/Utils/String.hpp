225f1ba16c8e84efc3bf5b836a9529bc6ef49d08
Dani <danixidev@gmail.com>
2024-07-29 21:30:54 +0200

    Check paths functions

--- src/Utils/String.hpp
+++ src/Utils/String.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <vector>
+#include <unistd.h>
+#include <iostream>
 
 template <typename T>
 std::vector<T>	splitSpaces(T& str)
@@ -29,6 +31,30 @@ std::vector<T>	splitSpaces(T& str)
 }
 
 template <typename T>
+std::vector<T>	splitChar(T& str, char c)
+{
+	std::vector<T>	result;
+	T					tmp;
+
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		while (str[i] == c)
+			i++;
+		while (str[i] != c && i < str.length())
+		{
+			tmp += str[i];
+			i++;
+		}
+		if (!tmp.empty())
+		{
+			result.push_back(tmp);
+			tmp.clear();
+		}
+	}
+	return result;
+}
+
+template <typename T>
 bool	onlySpaces(T& str)
 {
 	for (size_t i = 0; i < str.length(); i++)
@@ -36,6 +62,36 @@ bool	onlySpaces(T& str)
 		if (!std::isspace(str[i]))
 			return false;
 	}
+	return true;
+}
+
+template <typename T>
+bool	isValidRelativePath(T& str)
+{
+	if (str.empty())
+		return false;
+	if (str[0] == '/')
+		return false;
+	return isValidPath(str);
+}
+
+/**
+ * @brief Check is valid and exists (does not check permissions!)
+ * @param str The path to check
+ * @return true if the path is valid, false otherwise
+ */
+template <typename T>
+bool	isValidPath(T& str)
+{
+	if (str.empty())
+		return false;
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '/' && str[i + 1] == '/')
+			return false;
+	}
+	if (access(str.c_str(), F_OK) == -1)
+		return false;
 	return true;
 }
 
